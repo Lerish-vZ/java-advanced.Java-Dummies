d5e43e6a2d45af75a67d6a896778dc5c6abed340
Lerish-vZ <lerishavz@gmail.com>
2024-06-25 14:19:27 +0200

    update

--- book5/src/chapter1/version2/CountDownApp.java
+++ book5/src/chapter1/version2/CountDownApp.java
@@ -1,4 +1,17 @@
+/* LISTING 1-3 */
 package chapter1.version2;
 
+import chapter1.version1.CountDownClock;
+import chapter1.version1.LaunchEvent;
+
 public class CountDownApp {
+    public static void main(String[] args) {
+        Thread clock = new CountDownClock();
+        Runnable flood, ignition, liftoff;
+        flood = new LaunchEvent(16, "Flood the pad!");
+        ignition = new LaunchEvent(6, "Start engines!");
+        liftoff = new LaunchEvent(0, "Liftoff!");
+
+        
+    }
 }

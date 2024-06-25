861f35890390e777f0160d93d5bf2e0c814b2391
Lerish-vZ <lerishavz@gmail.com>
2024-06-25 14:36:10 +0200

    update

--- book5/src/chapter1/version3/CountDownApp.java
+++ book5/src/chapter1/version3/CountDownApp.java
@@ -1,4 +1,15 @@
+/* LISTING 1-4 */
 package chapter1.version3;
 
+import chapter1.version1.CountDownClock;
+
+import java.util.ArrayList;
+
 public class CountDownApp {
+    public static void main(String[] args) {
+        CountDownClock clock = new CountDownClock(20);
+        ArrayList<Runnable> events = new ArrayList<>();
+
+        
+    }
 }

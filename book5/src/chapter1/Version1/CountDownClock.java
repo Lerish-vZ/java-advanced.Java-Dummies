adf4b3c09309c54b316518b4513afdadb2599ac3
Lerish-vZ <lerishavz@gmail.com>
2024-06-25 13:59:43 +0200

    update

--- book5/src/chapter1/Version1/CountDownClock.java
+++ book5/src/chapter1/Version1/CountDownClock.java
@@ -2,15 +2,15 @@
 package chapter1.Version1;
 
 public class CountDownClock extends Thread {
-    public void run() {
+    public void run() { //This method is called by Java when the clock thread has been started. All processing done by the thread must be either in the run method or in some other method called by the run method.
         for (int t = 20; t >= 0; t--) {
             System.out.println("T minus " + t);
 
             try {
-                Thread.sleep(1000);
+                Thread.sleep(1000); // 1 second
             } catch (InterruptedException e) {
 
             }
         }
-    }
+    } //At some point in its execution, the run method must either call sleep or yield to give other threads a chance to execute
 }

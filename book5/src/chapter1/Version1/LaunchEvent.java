699c0622fefb3ad00d884da89655ee703da2652f
Lerish-vZ <lerishavz@gmail.com>
2024-06-25 14:14:33 +0200

    update

--- book5/src/chapter1/Version1/LaunchEvent.java
+++ book5/src/chapter1/Version1/LaunchEvent.java
@@ -11,9 +11,10 @@ public class LaunchEvent implements Runnable {
     }
 
     public void run() {
-        try{
-            Thread.sleep(20000 - (start * 1000));
-        } catch(InterruptedException e) {}
+        try {
+            Thread.sleep(20000 - (start * 1000)); //the countdown clock starts at 20 sec (20000 millisec.), meaning you can choose the desired statring time. Example if start = 6 sec the sleep method sleeps for 14000 milisec.
+        } catch (InterruptedException e) {
+        }
         System.out.println(message);
     }
 }

0895646b44e57808f4740960effb3dfdb5d35a96
Lerish-vZ <lerishavz@gmail.com>
2024-06-25 14:02:55 +0200

    update

--- book5/src/chapter1/Version1/CountDownApp.java
+++ book5/src/chapter1/Version1/CountDownApp.java
@@ -1,4 +1,8 @@
 package chapter1.Version1;
 
 public class CountDownApp {
+    public static void main(String[] args) {
+        Thread clock = new CountDownClock(); //creates Thread object but doesn't begin executing until you call its start method
+        clock.start();
+    }
 }

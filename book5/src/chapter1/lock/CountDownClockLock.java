0fc71356fbdcd4a39e8a95e2b5fb40c27cdbfba1
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 11:16:02 +0200

    update

--- book5/src/chapter1/lock/CountDownClockLock.java
+++ book5/src/chapter1/lock/CountDownClockLock.java
@@ -1,2 +1,25 @@
-package chapter1.lock;public class CountDownClockLock {
+package chapter1.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class CountDownClockLock extends Thread
+{
+    ReentrantLock lock = new ReentrantLock();
+
+    public void run()
+    {
+        lock.lock();
+        for (int t = 20; t >= 0; t--)
+        {
+            System.out.println("T minus " + t);
+            try
+            {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+
+            }
+        }
+        lock.unlock();
+    }
+
 }
--- book5/src/chapter1/lock/DoTwoThings.java
+++ book5/src/chapter1/lock/DoTwoThings.java
@@ -1,4 +1,9 @@
+/* LISTING 1-8 */
 package chapter1.lock;
 
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
 public class DoTwoThings {
+    ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(2);
+    CountDownClock
 }

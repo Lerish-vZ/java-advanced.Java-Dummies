622a7575146b49f0ce3f60972713fd06f9052baa
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 10:58:29 +0200

    update

--- book5/src/chapter1/newThreading/CountDownClock.java
+++ book5/src/chapter1/newThreading/CountDownClock.java
@@ -1,4 +1,13 @@
 package chapter1.newThreading;
 
-public class CountDownClock {
+public class CountDownClock implements Runnable{
+    int t;
+
+    public CountDownClock(int t){
+        this.t = t;
+    }
+
+    public void run(){
+        System.out.println("T minus " + t);
+    }
 }

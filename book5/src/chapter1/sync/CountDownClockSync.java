c31c57a6b35d0d34882e0e3491db3026073b9ec1
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 11:44:02 +0200

    update

--- book5/src/chapter1/sync/CountDownClockSync.java
+++ book5/src/chapter1/sync/CountDownClockSync.java
@@ -1,4 +1,21 @@
+/* LISTING 1-10 */
 package chapter1.sync;
 
-public class CountDownClockSync {
+public class CountDownClockSync extends Thread {
+    private int start;
+
+    public CountDownClockSync(int start) {
+        this.start = start;
+    }
+
+    synchronized public void run() {
+        for (int t = start; t > 0; t--) {
+            System.out.println("T minus " + t);
+            
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e)
+            {}
+        }
+    }
 }

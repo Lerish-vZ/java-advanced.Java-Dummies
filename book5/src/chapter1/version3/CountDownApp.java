aa6b313df23a4e48534361abce74d4fceb884da2
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 10:44:58 +0200

    update

--- book5/src/chapter1/version3/CountDownApp.java
+++ book5/src/chapter1/version3/CountDownApp.java
@@ -26,27 +26,7 @@ public class CountDownApp {
         int getTime();
     }
 
-    class CountDownClock extends Thread implements TimeMonitor {
-        private int t;
 
-        public CountDownClock(int start) {
-            this.t = start;
-        }
-
-        public void run() {
-            for (; t >= 0; t--) {
-                System.out.println("T minus " + t);
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception e) {
-                }
-            }
-        }
-
-        public int getTime() {
-            return t;
-        }
-    }
 
     class LaunchEvent implements Runnable {
         private int start;
--- book5/src/chapter1/version3/CountDownClock.java
+++ book5/src/chapter1/version3/CountDownClock.java
@@ -1,2 +1,23 @@
-package chapter1.version3;public class CountDownClock {
+package chapter1.version3;
+
+public class CountDownClock extends Thread implements CountDownApp.TimeMonitor {
+    private int t;
+
+    public CountDownClock(int start) {
+        this.t = start;
+    }
+
+    public void run() {
+        for (; t >= 0; t--) {
+            System.out.println("T minus " + t);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    public int getTime() {
+        return t;
+    }
 }

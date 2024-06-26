bd2bcd5eb467339ccfce9208c6e9abd55fd20fcb
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 11:20:05 +0200

    update

--- book5/src/chapter1/lock/DoTwoThings.java
+++ book5/src/chapter1/lock/DoTwoThings.java
@@ -1,9 +1,21 @@
 /* LISTING 1-8 */
 package chapter1.lock;
 
+import chapter1.newThreading.CountDownClock;
+
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 public class DoTwoThings {
     ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(2);
-    CountDownClock
+    CountDownClock clock = new CountDownClock(20);
+
+    public static void main(String[] args) {
+        new DoTwoThings();
+    }
+
+    DoTwoThings() {
+        pool.execute(clock);
+        pool.execute(clock);
+        pool.shutdown();
+    }
 }

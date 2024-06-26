2f53600f22f9718239ae56f1c8e25feea8dfca02
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 11:09:57 +0200

    update

--- book5/src/chapter1/newThreading/CountDownApp.java
+++ book5/src/chapter1/newThreading/CountDownApp.java
@@ -2,17 +2,25 @@
 package chapter1.newThreading;
 
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 class CountDownApp {
     public static void main(String[] args) {
-        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(25);
+        ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(25);
 
         Runnable flood, ignition, liftoff;
         flood = new LaunchEvent("Flood the pad!");
         ignition = new LaunchEvent("Start engines!");
         liftoff = new LaunchEvent("Liftoff!");
 
+        for (int t = 20; t > 0; t--) {
+            pool.schedule(new CountDownClock(t), (long)(20-t), TimeUnit.SECONDS);
+            pool.schedule(flood, 3L, TimeUnit.SECONDS);
+            pool.schedule(ignition, 13L, TimeUnit.SECONDS);
+            pool.schedule(liftoff, 19L, TimeUnit.SECONDS);
 
-        
+            pool.shutdown();
+        }
+
     }
 }

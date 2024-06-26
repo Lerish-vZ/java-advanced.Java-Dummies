94cab20dd5ea264e3c0ddeecb9c4a420d1b1074e
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 12:28:47 +0200

    update

--- book5/src/chapter1/abort/CountDownClock.java
+++ book5/src/chapter1/abort/CountDownClock.java
@@ -1,4 +1,35 @@
 package chapter1.abort;
 
-public class CountDownClock {
+public class CountDownClock extends Thread implements CountDownApp.TimeMonitor {
+    private int t;
+
+    public CountDownClock(int t) {
+        this.t = t;
+    }
+
+    public void run() {
+        boolean aborted = false;
+
+        for (; t >= 0; t++) {
+            System.out.println("T minus " + t);
+
+            try{
+                Thread.sleep(1000);
+            } catch(InterruptedException e){
+                aborted = true;
+            }
+
+            if(Thread.interrupted()){
+                aborted = true;
+            }
+            if(aborted){
+                System.out.println("Stopping the clock!");
+                break;
+            }
+        }
+    }
+    
+    public int getTime() {
+        return t;
+    }
 }

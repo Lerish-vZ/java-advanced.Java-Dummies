42365b94eebf6278f886adae914630a8de78a825
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 11:55:09 +0200

    update

--- book5/src/chapter1/lock/CountDownClockLocked.java
+++ book5/src/chapter1/lock/CountDownClockLocked.java
@@ -1,4 +1,20 @@
+/*  LISTING 1-12 */
 package chapter1.lock;
 
-public class CountDownClockLocked {
+import java.util.concurrent.locks.ReentrantLock;
+
+public class CountDownClockLocked extends Thread {
+    ReentrantLock lock = new ReentrantLock();
+
+    public void run(){
+        lock.lock();
+
+        for (int t = 20; t >= 0; t--) {
+            System.out.println("T minus " + t);
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {}
+        }
+        lock.unlock();
+    }
 }

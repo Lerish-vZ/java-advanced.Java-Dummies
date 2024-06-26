094a4c20e6b6e08b74b242ecf09ccf80d0122df2
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 11:50:59 +0200

    update

--- book5/src/chapter1/lock/DoTwoThingsLocked.java
+++ book5/src/chapter1/lock/DoTwoThingsLocked.java
@@ -1,4 +1,8 @@
+/* LISTING 1-11*/
 package chapter1.lock;
 
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
 public class DoTwoThingsLocked {
+    ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(2);
 }

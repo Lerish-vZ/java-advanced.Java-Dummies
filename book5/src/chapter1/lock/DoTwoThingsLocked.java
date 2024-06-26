3899d68a81591ddb5ff32a677a1b22feb492c001
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 11:53:21 +0200

    update

--- book5/src/chapter1/lock/DoTwoThingsLocked.java
+++ book5/src/chapter1/lock/DoTwoThingsLocked.java
@@ -6,5 +6,15 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 public class DoTwoThingsLocked {
     ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(2);
 
-    CountDownClockLocked clock = new CountDo
+    CountDownClockLocked clock = new CountDownClockLocked();
+
+    public static void main(String[] args) {
+        new DoTwoThingsLocked();
+    }
+
+    DoTwoThingsLocked() {
+        pool.execute(clock);
+        pool.execute(clock);
+        pool.shutdown();
+    }
 }

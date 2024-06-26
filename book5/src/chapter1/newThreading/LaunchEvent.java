b078daabdd54b59e2ae7374cb00d2b58a835f5a4
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 11:03:22 +0200

    update

--- book5/src/chapter1/newThreading/LaunchEvent.java
+++ book5/src/chapter1/newThreading/LaunchEvent.java
@@ -2,4 +2,13 @@
 package chapter1.newThreading;
 
 public class LaunchEvent implements Runnable {
+    private String message;
+
+    public LaunchEvent(String message) {
+        this.message = message;
+    }
+
+    public void run() {
+        System.out.println(message);
+    }
 }

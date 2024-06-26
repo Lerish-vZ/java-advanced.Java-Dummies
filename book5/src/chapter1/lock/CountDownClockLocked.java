21f17f73700c8e9cc811192d2cabdd13c4c10cbc
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 11:55:22 +0200

    update

--- book5/src/chapter1/lock/CountDownClockLocked.java
+++ book5/src/chapter1/lock/CountDownClockLocked.java
@@ -6,14 +6,15 @@ import java.util.concurrent.locks.ReentrantLock;
 public class CountDownClockLocked extends Thread {
     ReentrantLock lock = new ReentrantLock();
 
-    public void run(){
+    public void run() {
         lock.lock();
 
         for (int t = 20; t >= 0; t--) {
             System.out.println("T minus " + t);
-            try{
+            try {
                 Thread.sleep(1000);
-            } catch (InterruptedException e) {}
+            } catch (InterruptedException e) {
+            }
         }
         lock.unlock();
     }

86ad1128b91bfea679b07f73572e3c4a1363cc76
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 10:45:56 +0200

    update

--- book5/src/chapter1/version3/CountDownApp.java
+++ book5/src/chapter1/version3/CountDownApp.java
@@ -25,35 +25,4 @@ public class CountDownApp {
     interface TimeMonitor {
         int getTime();
     }
-
-
-
-    class LaunchEvent implements Runnable {
-        private int start;
-        private String message;
-
-        TimeMonitor tm;
-
-        public LaunchEvent(int start, String message, TimeMonitor monitor) {
-            this.start = start;
-            this.message = message;
-            this.tm = monitor;
-        }
-
-        public void run() {
-            boolean eventDone = false;
-
-            while (!eventDone) {
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                }
-
-                if (tm.getTime() <= start) {
-                    System.out.println(this.message);
-                    eventDone = true;
-                }
-            }
-        }
-    }
 }
--- book5/src/chapter1/version3/LaunchEvent.java
+++ book5/src/chapter1/version3/LaunchEvent.java
@@ -1,4 +1,30 @@
 package chapter1.version3;
 
-public class LaunchEvent {
+public class LaunchEvent implements Runnable {
+    private int start;
+    private String message;
+
+    CountDownApp.TimeMonitor tm;
+
+    public LaunchEvent(int start, String message, CountDownApp.TimeMonitor monitor) {
+        this.start = start;
+        this.message = message;
+        this.tm = monitor;
+    }
+
+    public void run() {
+        boolean eventDone = false;
+
+        while (!eventDone) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+            }
+
+            if (tm.getTime() <= start) {
+                System.out.println(this.message);
+                eventDone = true;
+            }
+        }
+    }
 }

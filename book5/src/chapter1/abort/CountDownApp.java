63248df9f69793fdfe845e54ff65dd502d41cf4b
Lerish-vZ <lerishavz@gmail.com>
2024-06-26 12:22:19 +0200

    update

--- book5/src/chapter1/abort/CountDownApp.java
+++ book5/src/chapter1/abort/CountDownApp.java
@@ -1,8 +1,14 @@
 /* LISTING 1-13 */
 package chapter1.abort;
 
+import java.util.ArrayList;
+
 public class CountDownApp {
     public static void main(String[] args) {
-        Count
+        CountDownClock clock = new CountDownClock(20);
+
+        ArrayList<Runnable> events = new ArrayList<>();
+
+        events.add(new Launch)
     }
 }
--- book5/src/chapter1/abort/LaunchEvent.java
+++ book5/src/chapter1/abort/LaunchEvent.java
@@ -1,2 +1,4 @@
-package chapter1.abort;public class LaunchEvent {
+package chapter1.abort;
+
+public class LaunchEvent {
 }

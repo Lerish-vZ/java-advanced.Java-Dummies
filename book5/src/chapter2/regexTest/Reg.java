252a7fe90c84b88da09c5825ba74e7e737985d1f
Lerish-vZ <lerishavz@gmail.com>
2024-07-01 12:03:02 +0200

    update

--- book5/src/chapter2/regexTest/Reg.java
+++ book5/src/chapter2/regexTest/Reg.java
@@ -1,4 +1,15 @@
 package chapter2.regexTest;
 
-public class Reg {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
+public final class Reg {
+    static String r, s; //static means that the variable belongs to the class itself rather than a specific instance of the class.
+    static Pattern pattern;
+    static Matcher matcher;
+    static boolean match, validRegex, doneMatching;
+    private static Scanner sc = new Scanner(System.in);
+
+    
+
 }

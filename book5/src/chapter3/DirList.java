587d1ad1ca3c6b1e814ab2ae55201bc607760466
Lerish-vZ <lerishavz@gmail.com>
2024-07-15 09:40:31 +0200

    update

--- book5/src/chapter3/DirList.java
+++ book5/src/chapter3/DirList.java
@@ -1,4 +1,24 @@
 package chapter3;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class DirList {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Welcome to the Directory Lister");
+
+        do {
+            System.out.print("\nEnter a path: ");
+            String path = sc.nextLine();
+            File dir = new File(path);
+
+            if(!dir.exists() || !dir.isDirectory())
+                System.out.println("\nThat directory does not exist.");
+            else {
+                System.out.println();
+            }
+        }
+    }
 }

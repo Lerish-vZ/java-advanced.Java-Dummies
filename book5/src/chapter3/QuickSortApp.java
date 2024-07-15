832f540825534fc5e472778084d65908b65c9b04
Lerish-vZ <lerishavz@gmail.com>
2024-07-15 11:19:03 +0200

    update

--- book5/src/chapter3/QuickSortApp.java
+++ book5/src/chapter3/QuickSortApp.java
@@ -44,4 +44,21 @@ public class QuickSortApp {
         sort(low,p);
         sort(p+1,high);
     }
+
+    public static int partition(int low, int high) {
+        int pivot = a[low];
+        int i = low - 1;
+        int j = high + 1;
+
+        while (i < j) {
+            for(i++; a[i] < pivot ; i++);
+            for(j--; a[j] > pivot; j--);
+
+            if(i < j)
+                swap(i, j)
+        }
+        return j;
+    }
+
+    
 }

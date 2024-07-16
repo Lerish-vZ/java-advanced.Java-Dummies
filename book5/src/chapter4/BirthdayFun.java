9db23b55e8909274213b26f83ece3a2e47257b02
Lerish-vZ <lerishavz@gmail.com>
2024-07-16 13:43:59 +0200

    update

--- book5/src/chapter4/BirthdayFun.java
+++ book5/src/chapter4/BirthdayFun.java
@@ -1,4 +1,27 @@
 package chapter4;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class BirthdayFun {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        do {
+            LocalDate birthDate;
+
+            DateTimeFormatter fullFormat = DateTimeFormatter.ofPattern("MMMM d, YYYY");
+            DateTimeFormatter monthDayFormat = DateTimeFormatter.ofPattern("MMMM d");
+
+            System.out.println("Today is " + LocalDate.now().format(fullFormat) + ".") ;
+
+            System.out.println();
+
+            System.out.print("Please enter your birthdate (yyyy-mmm-dd): ");
+            String input = sc.nextLine();
+
+            
+        }
+    }
 }

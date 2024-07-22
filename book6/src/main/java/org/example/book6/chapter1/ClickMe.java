6bfbb2e8ebff7589e86da107126a98c7756b5f49
Lerish-vZ <lerishavz@gmail.com>
2024-07-22 15:22:33 +0200

    update

--- book6/src/main/java/org/example/book6/chapter1/ClickMe.java
+++ book6/src/main/java/org/example/book6/chapter1/ClickMe.java
@@ -1,2 +1,43 @@
-package org.example.book6.chapter1;public class ClickMe {
+package org.example.book6.chapter1;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class ClickMe extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    Button btn;
+
+    public void start(Stage primaryStage) {
+        //create button
+        btn = new Button();
+        btn.setText("Click me please!");
+        btn.setOnAction(e -> buttonClick());
+
+        //Add the button to a layout pane
+        BorderPane pane = new BorderPane();
+        pane.setCenter(btn);
+
+        //Add the layout pane to a scene
+        Scene scene = new Scene(pane, 300, 250);
+
+        //Finalize and show the stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("The Click Me App");
+        primaryStage.show();
+    }
+
+    public void buttonClick(){
+        if(btn.getText() == "Click me please!"){
+            btn.setText("You clicked me!");
+        } else {
+            btn.setText("Click me please!");
+        }
+    }
 }
+

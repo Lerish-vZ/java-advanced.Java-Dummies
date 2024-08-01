4b776d3a9c766316aeedcfc43fcb8cf4c541a10d
Lerish-vZ <lerishavz@gmail.com>
2024-08-01 12:06:50 +0200

    update

--- book6/src/main/java/com/exp/book6/chapter1/ClickCounter.java
+++ book6/src/main/java/com/exp/book6/chapter1/ClickCounter.java
@@ -1,6 +1,9 @@
 package com.exp.book6.chapter1;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class ClickCounter extends Application {
@@ -9,8 +12,35 @@ public class ClickCounter extends Application {
         launch(args);
     }
 
+    Button btn;
+    Label lbl;
+    int iClickCounter = 0;
+
     @Override
     public void start(Stage primaryStage) {
+        //create button
+        btn = new Button();
+        btn.setText("Click me please!");
+        btn.setOnAction(e -> buttonClick());
 
+        //Add the button to a layout pane
+        BorderPane pane = new BorderPane();
+        pane.setCenter(btn);
+
+        //Add the layout pane to a scene
+        Scene scene = new Scene(pane, 300, 250);
+
+        //Finalize and show the stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("The Click Me App");
+        primaryStage.show();
+    }
+
+    public void buttonClick(){
+        if(btn.getText() == "Click me please!"){
+            btn.setText("You clicked me!");
+        } else {
+            btn.setText("Click me please!");
+        }
     }
 }

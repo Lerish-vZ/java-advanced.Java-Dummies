60d96892817b47a446b98c92988e9e96028d8d57
Lerish-vZ <lerishavz@gmail.com>
2024-08-02 10:44:20 +0200

    update

--- book6/src/main/java/com/exp/book6/chapter2/AddSubtract5.java
+++ book6/src/main/java/com/exp/book6/chapter2/AddSubtract5.java
@@ -13,8 +13,43 @@ public class AddSubtract5 extends Application {
         launch(args);
     }
 
+    Button btnAdd;
+    Button btnSubtract;
+    Label lbl;
+    int iCounter = 0;
+
     @Override
     public void start(Stage primaryStage) {
+        // Create the Add button
+        btnAdd = new Button();
+        btnAdd.setText("Add");
+        btnAdd.setOnAction(e -> {
+            iCounter++;
+            lbl.setText(String.valueOf(iCounter));
+        });
 
+        // Create the Subtract button
+        btnSubtract = new Button();
+        btnSubtract.setText("Subtract");
+        btnSubtract.setOnAction(e -> {
+            iCounter--;
+            lbl.setText(String.valueOf(iCounter));
+        });
+
+        // Create the Label
+        lbl = new Label();
+        lbl.setText(Integer.toString(iCounter));
+
+        // Add the buttons and label to an HBox pane
+        HBox pane = new HBox(10);
+        pane.getChildren().addAll(lbl, btnAdd, btnSubtract);
+
+        // Add the layout pane to a scene
+        Scene scene = new Scene(pane, 200, 75);
+
+        // Add the scene to the stage, set the title
+        // and show the stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Add/Sub");
+        primaryStage.show();
     }
-}

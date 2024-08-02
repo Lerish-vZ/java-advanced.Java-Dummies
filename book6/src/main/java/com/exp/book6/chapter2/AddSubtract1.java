f834a769a1d56ab8f07d6e09aa6336b5a46c15cc
Lerish-vZ <lerishavz@gmail.com>
2024-08-02 09:40:35 +0200

    update

--- book6/src/main/java/com/exp/book6/chapter2/AddSubtract1.java
+++ book6/src/main/java/com/exp/book6/chapter2/AddSubtract1.java
@@ -1,13 +1,20 @@
 package com.exp.book6.chapter2;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 
-public class AddSubtract1 extends Application {
+public class AddSubtract1 extends Application implements EventHandler <ActionEvent> {
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    Button btnAdd;
+    Button btnSubtract;
+    Label lbl;
+    int iCounter = 0;
 
     @Override
     public void start(Stage primaryStage) {

693cc3a0f397fcd6537c50b9ce7abb5f4c7aef52
Lerish-vZ <lerishavz@gmail.com>
2024-08-02 09:59:32 +0200

    update

--- book6/src/main/java/com/exp/book6/chapter2/AddSubtract2.java
+++ book6/src/main/java/com/exp/book6/chapter2/AddSubtract2.java
@@ -1,7 +1,11 @@
 package com.exp.book6.chapter2;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.event.*;
 
 public class AddSubtract2 extends Application {
 

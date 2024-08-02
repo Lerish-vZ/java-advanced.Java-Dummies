98f4432c7643a8db9b99acb7cd9a0bf92b93e34d
Lerish-vZ <lerishavz@gmail.com>
2024-08-02 10:38:45 +0200

    update

--- book6/src/main/java/com/exp/book6/chapter2/AddSubtract4.java
+++ book6/src/main/java/com/exp/book6/chapter2/AddSubtract4.java
@@ -1,6 +1,14 @@
 package com.exp.book6.chapter2;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.event.*;
 
 public class AddSubtract4 extends Application {
 
@@ -8,8 +16,50 @@ public class AddSubtract4 extends Application {
         launch(args);
     }
 
+    Button btnAdd;
+    Button btnSubtract;
+    Label lbl;
+    int iCounter = 0;
+
     @Override
     public void start(Stage primaryStage) {
-        
+        // Create the Add button
+        btnAdd = new Button();
+        btnAdd.setText("Add");
+        btnAdd.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                iCounter++;
+                lbl.setText(String.valueOf(iCounter));
+            }
+        });
+
+        // Create the Subtract button
+        btnSubtract = new Button();
+        btnSubtract.setText("Subtract");
+        btnSubtract.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                iCounter--;
+                lbl.setText(String.valueOf(iCounter));
+            }
+        });
+
+        // Create the Label
+        lbl = new Label();
+        lbl.setText(Integer.toString(iCounter));
+
+        // Add the buttons and label to an HBox pane
+        HBox pane = new HBox(10);
+        pane.getChildren().addAll(lbl, btnAdd, btnSubtract);
+
+        // Add the layout pane to a scene
+        Scene scene = new Scene(pane, 200, 75);
+
+        // Add the scene to the stage, set the title
+        // and show the stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Add/Sub");
+        primaryStage.show();
     }
 }
+

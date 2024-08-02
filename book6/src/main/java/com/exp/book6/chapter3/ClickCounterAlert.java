4af9bbe6a5801159dfd8b39b3e5ee1bd50069534
Lerish-vZ <lerishavz@gmail.com>
2024-08-02 11:47:53 +0200

    update

---
+++ book6/src/main/java/com/exp/book6/chapter3/ClickCounterAlert.java
@@ -0,0 +1,15 @@
+package com.exp.book6.chapter3;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClickCounterAlert extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}

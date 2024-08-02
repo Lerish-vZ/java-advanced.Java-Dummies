02a0e85f6f7526cd27c19271413f809774e144a1
Lerish-vZ <lerishavz@gmail.com>
2024-08-02 10:31:53 +0200

    update

--- book6/src/main/java/com/exp/book6/chapter2/AddSubtract3.java
+++ book6/src/main/java/com/exp/book6/chapter2/AddSubtract3.java
@@ -1,7 +1,11 @@
 package com.exp.book6.chapter2;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.event.*;
 
 public class AddSubtract3 extends Application {
 
@@ -9,8 +13,27 @@ public class AddSubtract3 extends Application {
         launch(args);
     }
 
+    Button btnAdd;
+    Button btnSubtract;
+    Label lbl;
+    int iCounter = 0;
+
     @Override
     public void start(Stage primaryStage) {
+// Create the Add button
+        btnAdd = new Button();
+        btnAdd.setText("Add");
+        btnAdd.setOnAction(new EventHandler<ActionEvent>() {
+                               @Override
+                               public void handle(ActionEvent e) {
+                                   iCounter++;
+                                   lbl.setText(String.valueOf(iCounter));
+                               }
+                           });
 
+                // Create the Subtract button
+                btnSubtract = new Button();
+        btnSubtract.setText("Subtract");
+        btnSubtract.setOnAction(       
     }
 }

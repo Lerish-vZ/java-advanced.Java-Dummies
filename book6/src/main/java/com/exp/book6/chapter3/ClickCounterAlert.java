13bb0f61e6f6c2b9e7b89ebb5f831f45436b25f1
Lerish-vZ <lerishavz@gmail.com>
2024-08-02 11:54:01 +0200

    update

--- book6/src/main/java/com/exp/book6/chapter3/ClickCounterAlert.java
+++ book6/src/main/java/com/exp/book6/chapter3/ClickCounterAlert.java
@@ -12,8 +12,34 @@ public class ClickCounterAlert extends Application {
         launch(args);
     }
 
+    Button btn;
+    Label lbl;
+    int iClickCount = 0;
+
     @Override
     public void start(Stage primaryStage) {
+        //Create the button
+        btn = new Button();
+        btn.setText("Click Me Please!");
+        btn.setOnAction(e -> buttonClick());
 
+        //Add the button to a layout pane
+        BorderPane pane = new BorderPane();
+        pane.setCenter(btn);
+
+        //Add the layout pane to a scene
+        Scene scene = new Scene(pane, 250, 150);
+
+        //Add the scene to the stage, set the title and show the stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Click Counter");
+        primaryStage.show();
+    }
+
+    public void buttonClick() {
+        iClickCount++;
+        if(iClickCount == 1) {
+            Alert a = new Alert(Alert.AlertType.INFORMATION, "You have clicked once.");
+        }
     }
 }

d02ed0bb80c0120ab49c1c79241d6e68e7ef2623
Lerish-vZ <lerishavz@gmail.com>
2024-08-02 10:29:07 +0200

    update

---
+++ book6/src/main/java/com/exp/book6/chapter2/AddSubtract3.java
@@ -0,0 +1,15 @@
+package com.exp.book6.chapter2;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AddSubtract3 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}

323179ac24b2596dbc0836e7a09ec9152b6f7731
Lerish-vZ <lerishavz@gmail.com>
2024-08-05 13:38:33 +0200

    update

--- book6/src/main/java/com/exp/book6/chapter4/PizzaOrder.java
+++ book6/src/main/java/com/exp/book6/chapter4/PizzaOrder.java
@@ -1,13 +1,30 @@
 package com.exp.book6.chapter4;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
 
 public class PizzaOrder extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    Stage stage;
+    TextField txtName;
+    TextField txtPhone;
+    TextField txtAdress;
+    RadioButton rdoSmall;
+    RadioButton rdoMedium;
+    RadioButton rdoLarge;
+    RadioButton rdoThin;
+    RadioButton rdoThick;
+    CheckBox chkPepperoni;
+    CheckBox chkMushrooms;
+    CheckBox chkAnchovies;
 
     @Override
     public void start(Stage primaryStage) {

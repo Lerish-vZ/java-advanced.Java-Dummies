0a08b04d6fe2b2c1b777be93eb72d2eed9d806de
Lerish-vZ <lerishavz@gmail.com>
2024-08-06 13:02:51 +0200

    update

--- book8/src/chapter1/FileVisitorDemo.java
+++ book8/src/chapter1/FileVisitorDemo.java
@@ -5,4 +5,16 @@ import java.io.*;
 import java.nio.file.attribute.*;
 
 public class FileVisitorDemo {
+    public static void main(String[] args) {
+        Path start = Paths.get("c:\\Windows\\System32");
+        MyFileVisitor visitor = new MyFileVisitor();
+
+        try {
+            Files.walkFileTree(start, visitor);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    
 }

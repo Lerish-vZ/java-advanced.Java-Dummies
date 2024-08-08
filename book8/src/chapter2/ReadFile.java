a44a33e5bc1d0cf8f97a6576ceff52bade39e847
Lerish-vZ <lerishavz@gmail.com>
2024-08-08 09:51:51 +0200

    update

--- book8/src/chapter2/ReadFile.java
+++ book8/src/chapter2/ReadFile.java
@@ -4,4 +4,14 @@ import java.io.*;
 import java.text.NumberFormat;
 
 public class ReadFile {
+    public static void main(String[] args) {
+        NumberFormat cf = NumberFormat.getInstance();
+        BufferedReader in = getReader("movies.txt");
+        Movies movie = readMovie(in);
+
+        while (movie != null) {
+            String msg = Integer.toString(movie.year);
+            
+        }
+    }
 }

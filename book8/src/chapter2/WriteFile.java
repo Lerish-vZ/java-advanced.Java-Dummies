16ebed426e1fd3d9b795995f9d8c305ac5cc704e
Lerish-vZ <lerishavz@gmail.com>
2024-08-08 11:13:43 +0200

    update

--- book8/src/chapter2/WriteFile.java
+++ book8/src/chapter2/WriteFile.java
@@ -5,6 +5,10 @@ import java.io.*;
 public class WriteFile {
     public static void main(String[] args) {
         Movie[] movies = getMovies();
-        
+        PrintWriter out = openWriter("C:\\YourJavaDirectory\\movies2.txt");
+
+        for(Movie m : movies)
+            writeMovie(m, out);
+        out.close();
     }
 }

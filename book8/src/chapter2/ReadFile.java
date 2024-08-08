46e329695caae3aa1a251f82d11051a41371a65c
Lerish-vZ <lerishavz@gmail.com>
2024-08-08 09:57:36 +0200

    update

--- book8/src/chapter2/ReadFile.java
+++ book8/src/chapter2/ReadFile.java
@@ -18,5 +18,16 @@ public class ReadFile {
         }
     }
 
-    
+    private static BufferedReader getReader(String name){
+        BufferedReader in = null;
+
+        try{
+            File file = new File(name);
+            in = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            System.out.println("File does not exist.");
+            System.exit(0);
+        }
+        return in;
+    }
 }

52be56b6eb6319d35f11dea7544f65872a6fd899
Lerish-vZ <lerishavz@gmail.com>
2024-08-12 14:46:07 +0200

    update

--- book8/src/chapter4/ListMovies.java
+++ book8/src/chapter4/ListMovies.java
@@ -4,5 +4,20 @@ import java.sql.*;
 import java.text.NumberFormat;
 
 public class ListMovies {
-    
+    public static void main(String[] args) {
+        NumberFormat cf = NumberFormat.getCurrencyInstance();
+        ResultSet movies = getMovies();
+
+        try {
+            while(movies.next()){
+                Movie m = getMovie(movies);
+                String msg = Integer.toString(m.year);
+                msg += ": " + m.title;
+                msg += " (" + cf.format(m.price + ")");
+                System.out.println(msg);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }

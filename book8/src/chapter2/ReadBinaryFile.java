1da7b67054d5aecbe1d2ff7a6df8392612777b5d
Lerish-vZ <lerishavz@gmail.com>
2024-08-12 09:26:28 +0200

    update

--- book8/src/chapter2/ReadBinaryFile.java
+++ book8/src/chapter2/ReadBinaryFile.java
@@ -6,6 +6,21 @@ import java.text.NumberFormat;
 public class ReadBinaryFile {
     public static void main(String[] args) {
         NumberFormat cf = NumberFormat.getCurrencyInstance();
-        
+        DataInputStream in = getStream("C:\\YourJavaDirectory\\movies.dat");
+        boolean eof = false;
+
+        while (!eof) {
+            Movie movie = readMovie(in);
+
+            if(movie = null)
+                eof = true;
+            else {
+                String msg = Integer.toString(movie.year);
+                msg += ": " + movie.title;
+                msg += " (" + cf.format(movie.price) + ")";
+                System.out.println(msg);
+            }
+        }
+        closeFile(in);
     }
 }

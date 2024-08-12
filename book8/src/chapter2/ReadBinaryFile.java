8fbdee2754b10807d0d1d3f2c9f10f21a83504d5
Lerish-vZ <lerishavz@gmail.com>
2024-08-12 09:23:27 +0200

    update

--- book8/src/chapter2/ReadBinaryFile.java
+++ book8/src/chapter2/ReadBinaryFile.java
@@ -1,4 +1,11 @@
 package chapter2;
 
+import java.io.*;
+import java.text.NumberFormat;
+
 public class ReadBinaryFile {
+    public static void main(String[] args) {
+        NumberFormat cf = NumberFormat.getCurrencyInstance();
+        
+    }
 }

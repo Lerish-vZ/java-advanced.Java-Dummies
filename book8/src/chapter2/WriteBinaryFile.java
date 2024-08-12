bb495a378a40d55c8b3a027d846f5fc26d6c4a90
Lerish-vZ <lerishavz@gmail.com>
2024-08-12 09:41:25 +0200

    update

--- book8/src/chapter2/WriteBinaryFile.java
+++ book8/src/chapter2/WriteBinaryFile.java
@@ -1,4 +1,16 @@
 package chapter2;
 
+import java.io.*;
+
 public class WriteBinaryFile {
+    public static void main(String[] args) {
+        Movie[] movies = getMovies();
+        DataOutputStream out = openOutputStream("C:\\YourJavaDirectory\\movies.dat");
+
+        for (Movie m : movies)
+            writeMovie(m, out);
+        closeFile(out);
+    }
+
+    
 }

5bc7a7abaaf7bccdf1961bbe4691456063d24b02
Lerish-vZ <lerishavz@gmail.com>
2024-08-13 11:23:17 +0200

    update

--- book8/src/chapter5/ListMoviesXML.java
+++ book8/src/chapter5/ListMoviesXML.java
@@ -24,4 +24,21 @@ public class ListMoviesXML {
             movieElement = (Element)movieElement.getNextSibling();
         }
     }
+
+    private static Document getDocument(String name){
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setIgnoringComments(true);
+            factory.setIgnoringElementContentWhitespace(true);
+            factory.setValidating(true);
+
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            return builder.parse(new InputSource(name));
+    } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    
 }

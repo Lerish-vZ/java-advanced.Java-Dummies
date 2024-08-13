9b4cdb9f8c91e0e4378ad6f846bae716e4ca782e
Lerish-vZ <lerishavz@gmail.com>
2024-08-13 11:18:26 +0200

    update

--- book8/src/chapter5/ListMoviesXML.java
+++ book8/src/chapter5/ListMoviesXML.java
@@ -1,9 +1,21 @@
 package chapter5;
 
-import javax.xml.parsers.*; 
+import javax.xml.parsers.*;
 import org.xml.sax.*;
 import org.w3c.dom.*;
 import java.text.*;
 
 public class ListMoviesXML {
+    private static NumberFormat cf = NumberFormat.getCurrencyInstance();
+
+    public static void main(String[] args) {
+        Document doc = getDocument("movies.xml");
+        Element root = doc.getDocumentElement();
+        Element movieElement = (Element)root.getFirstChild();
+        Movie m;
+
+        while(movieElement != null) {
+            
+        }
+    }
 }
